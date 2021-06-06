b71aba00b2943e2cd08189da642baebc0a4648d1
naotor <naoto.hori@gmail.com>
2021-06-07 06:25:23 +0900

    add navlink

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def navlink(title, path, current_controller, current_actions)
+    status = \
+      controller_name == current_controller.to_s && \
+      current_actions.include?(action_name.to_sym) ? \
+      "active" : ""
+
+    content_tag :li, class: status do
+      link_to title, path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'courses#index'
+
+  resources :courses
 end
