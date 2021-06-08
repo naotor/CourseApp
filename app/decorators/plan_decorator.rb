f37fd69b7cb53c550ec68928b719d5d64c7a0b39
naotor <naoto.hori@gmail.com>
2021-06-08 09:37:36 +0900

    partial plan

--- app/decorators/plan_decorator.rb
+++ app/decorators/plan_decorator.rb
@@ -1,11 +1,11 @@
 class PlanDecorator < ApplicationDecorator
   delegate_all
 
-  def display_title(i)
-    h.content_tag :b, "#%03d #{title}" % (i+1)
+  def display_title
+    h.content_tag :b, title
   end
 
   def display_body
-    h.simple_format(body)
+    h.simple_format body
   end
 end
