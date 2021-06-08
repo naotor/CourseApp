77c925713ee99de7e65853217a340b4f5998b9e9
naotor <naoto.hori@gmail.com>
2021-06-08 15:31:00 +0900

    refactoring

--- app/models/search/course.rb
+++ app/models/search/course.rb
@@ -7,21 +7,32 @@ class Search::Course < Search::Base
 
   def matches
     results = Course.all
+
+    # search: course_name
     results = results.where(
       contains(Course.arel_table[:name], course_name)
     ) if course_name.present?
+
+    # search: teacher_name
     results = results.joins(:teacher).where(
       contains(Teacher.arel_table[:name], teacher_name)
     ) if teacher_name.present?
+
+    # search: student_name
     results = results.joins(:students).where(
       contains(Student.arel_table[:name], student_name)
     ) if student_name.present?
+
+    # search: level_id
     results = results.where(
       level_id: level_id
     ) if level_id.present?
+
+    # search: genre_ids
     results = results.joins(:genres).where(
       AssignGenre.arel_table[:genre_id].in(genre_ids)
     ) if genre_ids.reject(&:blank?).present?
+
     results
   end
 end
